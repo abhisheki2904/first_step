3ec0f53334c03df8a22b71a15f734d948606e1b4
abhishekrudra <abhishek.k.sri2904@gmail.com>
2015-04-09 23:24:43 +0530

    df

--- src/com/rudra/first/Helloworld.java
+++ src/com/rudra/first/Helloworld.java
@@ -4,7 +4,7 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("mt First step");
+		System.out.println("my First step towards the git");
 
 	}
 
